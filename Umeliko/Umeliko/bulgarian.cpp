cd43724787fba8f21be31fb8ed7769401c027183
Dariya Kartalova <DYKartalova22@codingburgas.bg>
2024-04-21 15:55:35 +0300

    Edit Bulgarian page

--- Umeliko/Umeliko/bulgarian.cpp
+++ Umeliko/Umeliko/bulgarian.cpp
@@ -14,9 +14,11 @@ void bulgarian()
     bool buttonHover = false;
 
     const char* infoText1 = "Learn here:";
-
-
-
+    const char* infoText2 = "";
+    const char* infoText3 = "";
+    const char* infoText4 = "";
+    const char* infoText5 = "";
+    const char* infoText6 = "";
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
@@ -35,7 +37,11 @@ void bulgarian()
 
         DrawTextEx(customFont, "Bulgarian", Vector2{ 800, 50 }, 59, 2, WHITE);
         DrawTextEx(customFont, infoText1, Vector2{ 100, 280 }, 45, 2, DARKGREEN);  // Ot chisloto 45 se promenq razmera na teksta. Ako iskash da go napraish po maluk namalqsh nego
-
+        DrawTextEx(customFont, infoText2, Vector2{ 100, 280 }, 45, 2, DARKGREEN);
+        DrawTextEx(customFont, infoText3, Vector2{ 100, 280 }, 45, 2, DARKGREEN);
+        DrawTextEx(customFont, infoText4, Vector2{ 100, 280 }, 45, 2, DARKGREEN);
+        DrawTextEx(customFont, infoText5, Vector2{ 100, 280 }, 45, 2, DARKGREEN);
+        DrawTextEx(customFont, infoText6, Vector2{ 100, 280 }, 45, 2, DARKGREEN);
         DrawRectangleRec(testButton, buttonHover ? DARKGRAY : DARKGRAY);
 
         DrawTextEx(customFont, "Take a test", Vector2{ testButton.x + 10, testButton.y + 10 }, 33, 2, WHITE);
